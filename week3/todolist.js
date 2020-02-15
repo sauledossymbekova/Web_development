74ba533ef93c822ec783a65a4d913e07d773b500
Сауле Досымбекова <sauledosymbekova@MacBook-Air-Saule.local>
2020-02-15 14:16:31 +0600

    my-app

--- week3/todolist.js
+++ week3/todolist.js
@@ -2,7 +2,7 @@ var list = document.querySelector('ul');
 list.addEventListener('click', function (ev) {
     if(ev.target.tagName === "LI") {
        ev.target.classList.toggle('checked');
-    } else if(ev.target.tagName === "SPAN") {
+    } else if(ev.target.tagName === "IMG") {
        var div = ev.target.parentNode;
        div.remove();
     }
@@ -12,16 +12,15 @@ function newElement() {
     var li = document.createElement('li');
     var inputValue = document.getElementById('toDoEl').value;
     var t = document.createTextNode(inputValue);
-    li.appendChild(t);
+    li.appendChild(t); 
     if(inputValue == "") {
-       alert("Введите ваше дело!");
+       alert("Enter your task!");
     } else {
        document.getElementById('list').appendChild(li);
     }
     document.getElementById('toDoEl').value = "";
-    var span = document.createElement('SPAN');
-    var txt = document.createTextNode("X");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
+    var span = document.createElement("IMG");
+        span.setAttribute("src","https://cdn2.iconfinder.com/data/icons/e-business-helper/240/627249-delete3-512.png");
+        span.className = "close";
+        li.appendChild(span);
 }
